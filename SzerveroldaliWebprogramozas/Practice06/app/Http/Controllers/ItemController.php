91aed4fba65cc8bd787e71bf37a5490e7de12597
Tóta Dávid <totadavid95@gmail.com>
2020-03-23 10:02:17 +0100

    Update

--- SzerveroldaliWebprogramozas/Practice06/app/Http/Controllers/ItemController.php
+++ SzerveroldaliWebprogramozas/Practice06/app/Http/Controllers/ItemController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\ShoppingList;
+
 class ItemController extends Controller
 {
     public function __invoke($id) {
+        $shoppinglist = ShoppingList::where('id', $id)->first();
+
         return view('item')
                     ->with('id', $id)
-                    ->with(
+                    ->with('shoppinglist', $shoppinglist);
+
+                    /*->with(
                         'items', [
                             ['name' => 'Kakaó',     'quantity' => 3],
                             ['name' => 'Tej',       'quantity' => 2],
                             ['name' => 'Kenyér',    'quantity' => 1],
                         ]
-                    );
+                    );*/
     }
 }
--- SzerveroldaliWebprogramozas/Practice06/resources/views/item.blade.php
+++ SzerveroldaliWebprogramozas/Practice06/resources/views/item.blade.php
@@ -3,31 +3,39 @@
 @section('title', 'Lista megtekintése')
 
 @section('content')
-    <p>Megadott ID: {{ $id }}</p>
+    @if ($shoppinglist == null)
+        <div class="alert alert-danger text-center">
+            Az elem nem található
+        </div>
+    @else
+        <h1 class="text-center my-4">{{ $shoppinglist->name }}</h1>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Név</th>
-                <th scope="col">Mennyiség</th>
-                <th scope="col">Műveletek</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($items as $item)
-            <tr>
-                <th scope="row">{{ $loop->index + 1 }}.</th>
-                <td>{{ $item['name'] }}</td>
-                <td>{{ $item['quantity'] }} db</td>
-                <td>...</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Név</th>
+                    <th scope="col">Mennyiség</th>
+                    <th scope="col">Műveletek</th>
+                </tr>
+            </thead>
+            <tbody>
+                @if (isset($items))
+                    @foreach($items as $item)
+                    <tr>
+                        <th scope="row">{{ $loop->index + 1 }}.</th>
+                        <td>{{ $item['name'] }}</td>
+                        <td>{{ $item['quantity'] }} db</td>
+                        <td>...</td>
+                    </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
 
-    <div class="text-center my-4">
-        <a href="{{ route('new-item-index') }}" role="button" class="btn btn-lg btn-primary">Új elem</a>
-    </div>
+        <div class="text-center my-4">
+            <a href="{{ route('new-item-index') }}" role="button" class="btn btn-lg btn-primary">Új elem</a>
+        </div>
+    @endif
 
 @endsection
